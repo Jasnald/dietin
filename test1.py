dbba82ff77278c55f21f7944c12fde952ecff6c7
Guilherme NM <marchi.neves@gmail.com>
2026-02-08 17:24:28 +0100

    Update food macros and add new meal options in YAML files

---
+++ test1.py
@@ -0,0 +1,11 @@
+from calculator import calculate_nutrition
+
+my_day = [
+    
+    'MP_Arroz com Frango (1/5)',
+    'MP_Arroz com Frango (1/5)',
+    'Iogurte Turbinado', 
+    'Pao_com_ovo', 
+    'Whey_maquina'
+    ] 
+calculate_nutrition(my_day)
